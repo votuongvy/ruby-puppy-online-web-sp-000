2e8d3459e894a96a312d0639e06b312bbca3c152
vy vo <tuonggvy.vo@gmail.com>
2019-11-30 03:02:51 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
 # Add your code here
+class Dog 
+  
+  attr_accessor :name, :save
+ 
+  @@all = []
+ 
+  def initialize(name)
+    @name = name
+    save #call save method upon initilization
+  end
+  
+  def save #save method should be an instance method  class
+     @@all << self
+   end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+  
+end
