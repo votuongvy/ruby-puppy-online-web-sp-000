5efcbecc6e8acd2fafc29430fa7a49e1790b4069
vy vo <tuonggvy.vo@gmail.com>
2019-11-30 03:00:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -29,15 +29,3 @@ class Dog
   end
   
 end
-
-# class Album
-#   @@album_count = 0 
- 
-#   def initialize
-#     @@album_count += 1
-#   end
- 
-#   def self.count
-#     @@album_count
-#   end
-# end
