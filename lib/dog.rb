68653c1b141b7189673b99a2b7f002ed0b436738
vy vo <tuonggvy.vo@gmail.com>
2019-11-30 02:55:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,43 @@
 # Add your code here
+class Dog 
+  
+  attr_accessor :name
+ 
+  @@all = []
+ 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  # def self.save
+  #   @@all << self
+  # end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+  
+end
+
+# class Album
+#   @@album_count = 0 
+ 
+#   def initialize
+#     @@album_count += 1
+#   end
+ 
+#   def self.count
+#     @@album_count
+#   end
+# end
